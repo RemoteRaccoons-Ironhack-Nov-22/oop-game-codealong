3175f97e9b76f701f8004c1aa6df511a11705a8a
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-12-01 17:46:53 +0100

    create and move obstacles + collision detection

--- js/main.js
+++ js/main.js
@@ -36,11 +36,45 @@ class Player {
     }
 }
 
+class Obstacle {
+    constructor() {
+        this.width = 20;
+        this.height = 10;
+        this.positionX = 50 - (this.width /2);
+        this.positionY = 80;
+
+        this.domElement = null;
+        this.createDomElement();
+    }
+    createDomElement(){
+        // step1: create the element:
+        this.domElement = document.createElement('div');
+
+        // step2: add content or modify (ex. innerHTML...)
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        //step3: append to the dom: `parentElm.appendChild()`
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement);
+    }
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+}
+
 //////////////////////////
 
 
 const player = new Player();
+const obstacles = []; //will hold instances of the class Obstacle
 
+
+//Attach event listeners
 document.addEventListener('keydown', function(event) {
     if(event.key === "ArrowRight"){
         player.moveRight();
@@ -48,4 +82,34 @@ document.addEventListener('keydown', function(event) {
         player.moveLeft();
     }
 });
+
+
+// Create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstacles.push(newObstacle);
+}, 3000);
+
+
+
+
+//Move obstacles & detect collision
+setInterval(() => {
+    obstacles.forEach( (obstacleInstance) => {
+
+        //move current obstacle
+        obstacleInstance.moveDown();
+
+        //detect if there's a collision between player and current obstacle
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.height + player.positionY > obstacleInstance.positionY
+        ) {
+            console.log("collision detected!!");
+        }
+    });
+}, 50)
+
 
